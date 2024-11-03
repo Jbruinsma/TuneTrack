59f8aa0660261a5e135756e96429f12653972160
Jbruinsma <146146472+Jbruinsma@users.noreply.github.com>
2024-11-02 20:02:41 -0400

    Update form.py

--- form.py
+++ form.py
@@ -1,18 +1,21 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, SelectMultipleField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 class PlaylistForm(FlaskForm):
     # The image field allows only image formats (jpg, png, jpeg)
     playlist_image = FileField(validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
     # Playlist name is required
     playlist_name = StringField(validators=[DataRequired()])
-    # At least one song must be selected
-    selected_music = SelectMultipleField('Select Music', choices=[], validators=[DataRequired()])
+    # Submit button
     submit = SubmitField('Create Playlist')
 
-    # Custom validation to ensure at least one song is selected
-    def validate_selected_music(self, field):
-        if not field.data:
-            raise ValidationError('You must select at least one song for the playlist.')
+class EditingForm(FlaskForm):
+        # The image field allows only image formats (jpg, png, jpeg)
+        playlist_image = FileField(validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+        # Playlist name is required
+        playlist_name = StringField(validators=[DataRequired()])
+        # Submit button
+        submit = SubmitField('Edit Playlist')
+    
